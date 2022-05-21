9f2dee1b61b70052d501d1464d4b06a530ac049d
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-05-21 16:44:19 +0500

    js basics

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log("Hello World");
+
+// document.write("Hello World");
+
+// alert("Hello World");
+
+var firstName = "Hanzala";
+var lastName = "Shakeel";
+var a = "SMith";
+var b = "Pointer";
+var c = 1;
+var d = 10;
+
+console.log("A Value", a);
+console.log("B Value", b);
+console.log("C Value", c);
+console.log("D Value", d);
+
+console.log("Hanzala Shakeel");
+console.log(firstName + " " + lastName);
+
+console.log("For Mathematical Operation", c + d);
+console.log("For Mathematical Operation", c * d);
+console.log("For Mathematical Operation", c - d);
+console.log("For Mathematical Operation", c / d);
+console.log("For Mathematical Operation", c % d);
+
+var userInput = prompt("ENter Your Name");
+
+document.write(userInput);
+
+console.log(typeof userInput);
+
+// Return Types in JS
+
+// Primitive Data Type: String Number Boolean Undefined Null
+
+// parseInt , Number , + , parseFloat
+
+// Non Primitive Referencing: Array Objects
