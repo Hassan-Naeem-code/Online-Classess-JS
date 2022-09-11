c06c3db1b79235b0ee21c26648546d398801e2f7
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-09-11 12:48:24 +0500

    code updated with edit functionality

--- Class 17/index.js
+++ Class 17/index.js
@@ -2,9 +2,11 @@
 const database = firebase.firestore();
 let myName = document.getElementById("name");
 let fatherName = document.getElementById("fatherName");
+let getSubmitButton = document.getElementById("get-submit-button");
 const getParentElementToPrintItChildUsers =
   document.getElementById("form-group");
 let unsubscribe;
+let editElementId;
 
 window.addEventListener("load", () => {
   unsubscribe = database.collection("users").onSnapshot((snapshot) => {
@@ -18,6 +20,10 @@ window.addEventListener("load", () => {
       }
       if (change.type === "modified") {
         console.log("Modified users: ", change.doc.data());
+        const dataFromFirebase = change.doc.data();
+        if (dataFromFirebase) {
+          printUiForUsers(dataFromFirebase);
+        }
       }
       if (change.type === "removed") {
         const dataFromFirebase = change.doc.data();
@@ -33,16 +39,22 @@ window.addEventListener("load", () => {
 
 const printUiForUsers = (dataFromFirebase) => {
   const parentElement = document.createElement("div");
-  const makeImageElement = document.createElement("img");
-  makeImageElement.setAttribute("src", "assets/edit.png");
-  makeImageElement.setAttribute("class", "img-responsive");
-  makeImageElement.setAttribute("onclick", "pressDeleteUser(this)");
-  makeImageElement.setAttribute("id", dataFromFirebase.id);
+  const makeImageElementForEdit = document.createElement("img");
+  makeImageElementForEdit.setAttribute("src", "assets/edit.png");
+  makeImageElementForEdit.setAttribute("class", "img-responsive");
+  makeImageElementForEdit.setAttribute("onclick", "pressEditUser(this)");
+  makeImageElementForEdit.setAttribute("id", dataFromFirebase.id);
+  const makeImageElementForDelete = document.createElement("img");
+  makeImageElementForDelete.setAttribute("src", "assets/trash-bin.png");
+  makeImageElementForDelete.setAttribute("class", "img-responsive");
+  makeImageElementForDelete.setAttribute("onclick", "pressDeleteUser(this)");
+  makeImageElementForDelete.setAttribute("id", dataFromFirebase.id);
   const childElement = document.createElement("h2");
   const secondChildElement = document.createElement("h3");
-  childElement.append("Name is: ", dataFromFirebase?.name);
-  secondChildElement.append("Father Name is: ", dataFromFirebase?.fatherName);
-  parentElement.appendChild(makeImageElement);
+  childElement.append(dataFromFirebase?.name);
+  secondChildElement.append(dataFromFirebase?.fatherName);
+  parentElement.appendChild(makeImageElementForEdit);
+  parentElement.appendChild(makeImageElementForDelete);
   parentElement.appendChild(childElement);
   parentElement.appendChild(secondChildElement);
   getParentElementToPrintItChildUsers.appendChild(parentElement);
@@ -61,6 +73,39 @@ const pressDeleteUser = (dataForUser) => {
     })
     .catch((error) => {
       console.error("Error removing document: ", error);
+    });
+};
+const pressEditUser = (user) => {
+  console.log("user is", user);
+  const user_id = user?.id;
+  const elementParent = user.parentElement;
+  const elementFirstName = elementParent.childNodes[2].innerHTML;
+  const elementLastName = elementParent.childNodes[3].innerHTML;
+  myName.value = elementFirstName;
+  fatherName.value = elementLastName;
+  getSubmitButton.innerHTML = "Edit User";
+  getSubmitButton.setAttribute("onclick", "editUser()");
+  editElementId = user_id;
+  getParentElementToPrintItChildUsers.removeChild(elementParent);
+};
+const editUser = () => {
+  database
+    .collection("users")
+    .doc(editElementId)
+    .set({
+      name: myName.value,
+      fatherName: fatherName.value,
+      id: editElementId,
+    })
+    .then((doc) => {
+      myName.value = "";
+      fatherName.value = "";
+      console.log("error is", doc);
+      getSubmitButton.innerHTML = "Send User";
+      getSubmitButton.setAttribute("onclick", "sendData()");
+    })
+    .catch((error) => {
+      console.log("error is", error);
     });
 };
 const sendData = () => {
