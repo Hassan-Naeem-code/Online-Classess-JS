0303d7fd6313e62ac6e2c87f5010d677f58d26fb
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-12-18 16:28:58 +0500

    promises in javascript

--- Class 20/index.js
+++ Class 20/index.js
@@ -19,3 +19,43 @@ function callApi() {
     .then((yasir) => console.log("response", yasir))
     .catch((error) => console.log("error", error));
 }
+
+function colorTheBlock(backgroundColor,color, text) {
+  const captureTheBlock = document.getElementById("demo");
+  captureTheBlock.style.backgroundColor = backgroundColor;
+  captureTheBlock.style.color = color;
+  captureTheBlock.innerHTML = text;
+}
+// promise
+
+let promise = new Promise(function (resolve, reject) {
+  // if condition run successfully then resolve will automatically called
+  let x = 0;
+  if (x == 0) {
+    let referenceObject = {
+      backgroundColor: "green",
+      color: "white",
+      text: "condition is true",
+    };
+    resolve(referenceObject);
+  }
+  // if condition donot run successfully then reject will be called automatically
+  else {
+    let referenceObject = {
+      backgroundColor: "red",
+      color: "white",
+      text: "condition is false",
+    };
+    reject(referenceObject);
+  }
+});
+
+promise
+  .then((resolve) => {
+    console.log("resolve", resolve?.text);
+    colorTheBlock(resolve?.backgroundColor, resolve?.color, resolve?.text);
+  })
+  .catch((reject) => {
+    console.log("reject", reject?.text);
+    colorTheBlock(resolve?.backgroundColor, reject?.color, reject?.text);
+  });
